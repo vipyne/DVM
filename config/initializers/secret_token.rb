9a9fe88fc0acfd1ce39c2334171db131b596653c
vanessa <vipyne@gmail.com>
2013-11-20 14:56:17 -0800

    initial commit - rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PimpMyMeme::Application.config.secret_token = '2cc6e12c475f9bae8587e14d3f1261adc53b0e4a7b3313d047f51a2605aa9bf0d6c318f481057c8e160397f358cfc376fdbb3cdd13f9bdc4d1d64956f7b92eaa'
