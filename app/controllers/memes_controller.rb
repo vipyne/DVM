5705e02fd575c03b2eb242cafbcc8271f6f24177
vanessa <vipyne@gmail.com>
2013-11-21 12:00:48 -0800

    beginning to master cloudinary... booyah, eh.

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -2,6 +2,18 @@ class MemesController < ApplicationController
 
   def index
     @memes = Meme.all
+    @meme = Meme.new
+  end
+
+  def create
+    clres = Cloudinary::Uploader.upload(params[:meme][:image_url])
+    p clres
+    @meme = Meme.create(title: params[:meme][:title], image_url: clres["url"])
+    redirect_to meme_path(@meme)
+  end
+
+  def new
+    @meme = Meme.new
   end
 
   def show
