7c49f528b6cd97ef528622ab28d69948cf9134b6
Apprentice <apprentice@dbc-23.local>
2013-11-20 19:05:36 -0800

    Added functionality to the captions.  They can be displayed on the meme's show page, and they get there as a partial.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 PimpMyMeme::Application.routes.draw do
-<<<<<<< HEAD
-  resources :memes, :captions
-
-=======
 
   root :to => "memes#index"
 
   resources :memes, :captions
 
 
->>>>>>> 984c359255c714f9fe9f0aa69ed8339d90c26230
 end
