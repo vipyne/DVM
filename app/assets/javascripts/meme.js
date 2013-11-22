de2d8f466f9967fbda2d85ceaa8f943378fe5b92
Apprentice <apprentice@dbc-22.local>
2013-11-22 11:13:27 -0800

    Fixed double partial render, added text to images, and vote counter styles to caption table. Modified CSS and on show page images now have div matching the image size to contain the meme text.

--- app/assets/javascripts/meme.js
+++ app/assets/javascripts/meme.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
 
-  $(".caption_form").on('submit',function(e){
+
+
+  $(".caption_form form").on('submit',function(e){
     e.preventDefault()
+
+    var form = $(this);
 
     $.ajax({
       url: '/captions',
       type: 'POST',
       data: $(this).serialize()
     }).done(function(server_data){
-      $("table").append(server_data)
-      $("#caption_title").val("")
-      $("#caption_signature").val("")
+      $(".caption_table table").append(server_data)
+      form.find("textarea, input[type=text]").val("");
     })
 
   })
