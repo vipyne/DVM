dd9f0432b42676d91d477579c46f3ab070e08514
vanessa <vipyne@gmail.com>
2013-11-22 17:16:44 -0800

    css and animate new meme button and form

--- app/assets/javascripts/new-meme.js
+++ app/assets/javascripts/new-meme.js
@@ -3,12 +3,15 @@ $(document).ready(function(){
 
 $('.visible').hide()
 
-$('.add-a-meme').on('click', function(e){
+$('.add-a-meme a').on('click', function(e){
   e.preventDefault()
   console.log("wtf")
-    $('.visible').show()
-  
+    $('.visible').slideDown()
 })
+
+
+
+
 
 
 
@@ -16,3 +19,12 @@ $('.add-a-meme').on('click', function(e){
 
 /////////////////////////////
 })
+
+
+// <a href="http://www.cornify.com"
+// onclick="cornify_add();return false;">
+// <img src="http://www.cornify.com/assets/cornify.gif"
+// width="61" height="16" border="0" alt="Cornify" />
+// </a>
+
+// <script type="text/javascript" src="http://www.cornify.com/js/cornify.js"></script>
