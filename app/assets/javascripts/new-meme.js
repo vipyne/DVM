f1f3bb66a990a07779e22f00adee6d4de02e0091
vanessa <vipyne@gmail.com>
2013-11-24 15:31:15 -0800

    image size stuff... still debugging

--- app/assets/javascripts/new-meme.js
+++ app/assets/javascripts/new-meme.js
@@ -10,8 +10,8 @@ $('.add-a-meme a').on('click', function(e){
 })
 
 // containers for impact caption text
-var actualPicHeight = $('img.meme_show').height()
 var actualPicWidth = $('img.meme_show').width()
+var actualPicHeight = $('img.meme_show').height()
 
 var picHeightBefore = $('div.height-fix').text()
 var picHeight = picHeightBefore - 80
@@ -28,13 +28,3 @@ console.log('meme_show pic height', actualPicWidth, actualPicHeight)
 /////////////////////////////
 })
 
-////// possible future easter egg?? ///////////////
-///////////////////////////////////////////////////
-// <a href="http://www.cornify.com"
-// onclick="cornify_add();return false;">
-// <img src="http://www.cornify.com/assets/cornify.gif"
-// width="61" height="16" border="0" alt="Cornify" />
-// </a>
-
-// <script type="text/javascript" src="http://www.cornify.com/js/cornify.js"></script>
-///////////////////////////////////////////////////
