7f44cf7ee087e137874db44c12491820de95b06e
vanessa <vipyne@gmail.com>
2013-11-24 13:30:08 -0800

    old way of capturing pic data... idk why i was making it so complicated

--- app/assets/javascripts/new-meme.js
+++ app/assets/javascripts/new-meme.js
@@ -10,16 +10,20 @@ $('.add-a-meme a').on('click', function(e){
 })
 
 // containers for impact caption text
+var actualPicHeight = $('img.meme_show').height()
+var actualPicWidth = $('img.meme_show').width()
+
 var picHeightBefore = $('div.height-fix').text()
 var picHeight = picHeightBefore - 80
-$('div.image').css('height', picHeight)
+$('div.image').css('height', actualPicHeight)
 
 var picWidthBefore = $('div.width-fix').text()
 var picWidth = picWidthBefore - 80
-$('div.image').css('width', picWidth)
+$('div.image').css('width', actualPicWidth)
 
-console.log('hBB:', picHeightBefore,'wBB:', picWidthBefore)
-console.log('h:', picHeight,'w:', picWidth)
+console.log('wBB:', picWidthBefore, 'hBB:', picHeightBefore)
+console.log('w:', picWidth,'h:', picHeight)
+console.log('meme_show pic height', actualPicWidth, actualPicHeight)
 
 /////////////////////////////
 })
