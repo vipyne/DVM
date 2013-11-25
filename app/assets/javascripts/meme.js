fab59ab66dbb238af78f43f8757a4e98e5a1b3b0
vanessa <vipyne@gmail.com>
2013-11-24 17:53:39 -0800

    immediate captions and views working

--- app/assets/javascripts/meme.js
+++ app/assets/javascripts/meme.js
@@ -14,9 +14,9 @@ $(document).ready(function(){
       type: 'POST',
       data: $(this).serialize()
     }).done(function(data){
-      $("tbody").append(data)
+      $(".caption_table table").append(data)
       form.find("textarea, input[type=text]").val("");
-      console.log($(".image p").text())
+      // console.log($(".image p").text())
       if ($(".image p").text() == "") {
         $(".image p").text(title)
       }
@@ -27,13 +27,13 @@ $(document).ready(function(){
   var orig_text = $(".image p").text()
 
 
-  $("tbody").on('mouseover', 'tr', function(e){
+  $(".caption_table table").on('mouseover', 'tr', function(e){
     e.preventDefault
     var meme_text = $(this).children().eq(1).text()
     $(".image p").text(meme_text)
   })
 
-  $("tbody").on('mouseout','tr', function(e){
+  $(".caption_table table").on('mouseout','tr', function(e){
     e.preventDefault
     $(".image p").text(orig_text)
   })
