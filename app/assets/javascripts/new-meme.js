69b7f6baaf9135934c3c23afc2e9057fc1b0787c
vanessa <vipyne@gmail.com>
2013-11-24 19:30:18 -0800

    voting!  it works (now you vote lots)

--- app/assets/javascripts/new-meme.js
+++ app/assets/javascripts/new-meme.js
@@ -13,16 +13,17 @@ $('.add-a-meme a').on('click', function(e){
 var actualPicWidth = $('img.meme_show').width()
 var actualPicHeight = $('img.meme_show').height()
 
-var picHeightBefore = $('div.height-fix').text()
-var picHeight = picHeightBefore - 80
+// var picHeightBefore = $('div.height-fix').text()
+// var picHeight = picHeightBefore - 80
+// $('div.image').css('height', picHeight)
 $('div.image').css('height', actualPicHeight)
 
-var picWidthBefore = $('div.width-fix').text()
-var picWidth = picWidthBefore - 80
+// var picWidthBefore = $('div.width-fix').text()
+// var picWidth = picWidthBefore - 80
 $('div.image').css('width', actualPicWidth)
 
-console.log('wBB:', picWidthBefore, 'hBB:', picHeightBefore)
-console.log('w:', picWidth,'h:', picHeight)
+// console.log('wBB:', picWidthBefore, 'hBB:', picHeightBefore)
+// console.log('w:', picWidth,'h:', picHeight)
 console.log('meme_show pic height', actualPicWidth, actualPicHeight)
 
 /////////////////////////////
--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
 ///////////////////////////////////////////////
 
-  $(".vote_up_button").on("click", function(e){
+  $(".caption_table table").on("click", "button.vote_up_button", function(e){
     e.preventDefault()
+    console.log($('.vote_up_button button'))
+    console.log('this serialized:',$(this).parent().text())
+
     var thisThing = $(this).parent()
+    var votes = Number(thisThing.children().text())
     var id = $(this).attr("id")
     $.ajax({
       url: "/captions/"+id,
       type: "PUT"
     }).done(function(server_data){
+      votes += 1
       var grandParent = thisThing.parent()
       grandParent.parent().replaceWith(server_data)
+      console.log(grandParent.parent())
+
       // location.reload()
     })
   })
